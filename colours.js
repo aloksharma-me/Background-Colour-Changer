6cd31995d0620f633489ef2826a73de6f6d3f929
aloksharma-me <alok.sharma104@gmail.com>
2024-07-25 18:02:45 +0530

    needs some bug fixing

---
+++ colours.js
@@ -0,0 +1,53 @@
+const colours = [
+    {
+        name: "deepOrange",
+        hex: "#F52F1D"
+    },
+
+    {
+        name: "lightBlue",
+        hex: "#4F68F1"
+    },
+
+    {
+        name: "pink",
+        hex: "#F45191"
+    },
+
+    {
+        name: "orange",
+        hex: "#FC602B"
+    },
+
+    {
+        name: "darkRed",
+        hex: "#A51507"
+    },
+
+    {
+        name: "darkBlue",
+        hex: "#0040AA"
+    },
+
+    {
+        name: "darkGreen",
+        hex: "#005646"
+    },
+
+    {
+        name: "purple",
+        hex: "#3F0589"
+    },
+
+    {
+        name: "deepGold",
+        hex: "#7E6408"
+    },
+
+    {
+        name: "deepBlue",
+        hex: "#041184"
+    },
+];
+
+export default colours;
---
+++ script.js
@@ -0,0 +1,3 @@
+import bgColour from "./colours";
+
+console.log(bgColour.colours[0]);
