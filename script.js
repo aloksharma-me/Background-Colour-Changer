305f22d7d702bb34da7adecd89e0c14ba75c964c
aloksharma-me <alok.sharma104@gmail.com>
2024-07-26 07:41:45 +0530

    added button interactions

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
-import bgColour from "./colours";
+const colours = [
+    {
+        name: "deepOrange",
+        hex: "#F52F1D"
+    },
 
-console.log(bgColour.colours[0]);
+    {
+        name: "lightBlue",
+        hex: "#4F68F1"
+    },
+
+    {
+        name: "pink",
+        hex: "#F45191"
+    },
+
+    {
+        name: "orange",
+        hex: "#FC602B"
+    },
+
+    {
+        name: "darkRed",
+        hex: "#A51507"
+    },
+
+    {
+        name: "darkBlue",
+        hex: "#0040AA"
+    },
+
+    {
+        name: "darkGreen",
+        hex: "#005646"
+    },
+
+    {
+        name: "purple",
+        hex: "#3F0589"
+    },
+
+    {
+        name: "deepGold",
+        hex: "#7E6408"
+    },
+
+    {
+        name: "deepBlue",
+        hex: "#041184"
+    },
+];
+
+let changeColourButton = document.querySelector("button");
+
+function changeColour(){
+    console.log("Changed Colour!!!");
+}
+
+changeColourButton.onclick = changeColour();
