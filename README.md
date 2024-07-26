eb1469604880ffec7b1aa37faab7b5ed96abaa32
aloksharma-me <alok.sharma104@gmail.com>
2024-07-26 10:25:12 +0530

    added a readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+### Colour Changer
+This app generates random background colours with each click, perfect for design inspiration, mood setting, or just a splash of fun.
+
+Simple, intuitive, and endlessly colourful.
--- script.js
+++ script.js
@@ -1,56 +1,17 @@
 const colours = [
-    {
-        name: "deepOrange",
-        hex: "#F52F1D"
-    },
-
-    {
-        name: "lightBlue",
-        hex: "#4F68F1"
-    },
-
-    {
-        name: "pink",
-        hex: "#F45191"
-    },
-
-    {
-        name: "orange",
-        hex: "#FC602B"
-    },
-
-    {
-        name: "darkRed",
-        hex: "#A51507"
-    },
-
-    {
-        name: "darkBlue",
-        hex: "#0040AA"
-    },
-
-    {
-        name: "darkGreen",
-        hex: "#005646"
-    },
-
-    {
-        name: "purple",
-        hex: "#3F0589"
-    },
-
-    {
-        name: "deepGold",
-        hex: "#7E6408"
-    },
-
-    {
-        name: "deepBlue",
-        hex: "#041184"
-    },
+    {name: "deepOrange", hex: "#F52F1D"},
+    {name: "lightBlue", hex: "#4F68F1"},
+    {name: "pink", hex: "#F45191"},
+    {name: "orange", hex: "#FC602B"},
+    {name: "darkRed", hex: "#A51507"},
+    {name: "darkBlue", hex: "#0040AA"},
+    {name: "darkGreen", hex: "#005646"},
+    {name: "purple", hex: "#3F0589"},
+    {name: "deepGold", hex: "#7E6408"},
+    {name: "deepBlue", hex: "#041184"},
 ];
 
-let changeColourButton = document.querySelector("button");
+let changeColourButton = document.querySelector("#btn");
 
 function changeColour(){
     console.log("Changed Colour!!!");
