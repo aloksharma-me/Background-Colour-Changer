7c66974c87435b79e0d63d2a1a644337d533762a
aloksharma-me <alok.sharma104@gmail.com>
2024-07-26 16:42:20 +0530

    added freeCodeCamp's version of colour changer JavaScript code

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+//  freeCodeCamp version of the JavaScript code
+//  const darkColorsArr = [
+//     "#2C3E50",
+//     "#34495E",
+//     "#2C2C2C",
+//     "#616A6B",
+//     "#4A235A",
+//     "#2F4F4F",
+//     "#0E4B5A",
+//     "#36454F",
+//     "#2C3E50",
+//     "#800020",
+//   ];
+  
+//   function getRandomIndex() {
+//     const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+//     return randomIndex;
+//   }
+  
+//   const body = document.querySelector("body");
+//   const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+  
+//   function changeBackgroundColor() {
+//     const color = darkColorsArr[getRandomIndex()];
+  
+//     bgHexCodeSpanElement.innerText = color;
+//     body.style.backgroundColor = color;
+//   }
+//   const btn = document.querySelector("#btn");
+  
+//   btn.onclick = changeBackgroundColor();
+
+
 const colours = [
     {name: "deepOrange", hex: "#F52F1D"},
     {name: "lightBlue", hex: "#4F68F1"},
@@ -15,11 +48,12 @@ let button = document.querySelector("#btn");
 let colourHex = document.querySelector("#colourHex");
 let backgroundColour = document.querySelector(".rightContainer");
 let insideBackgroundColour = document.querySelector(".colourInsideContainer");
+const length = colours.length;
 
 button.onclick = changeColour;
 
 function changeColour(){
-    let index = Math.ceil(Math.random()*(9-0))
+    let index = Math.ceil(Math.random()*(length - 0));
     backgroundColour.style.backgroundColor = colours[index].hex;
     insideBackgroundColour.style.backgroundColor = colours[index].hex;
     colourHex.innerText = colours[index].hex;
